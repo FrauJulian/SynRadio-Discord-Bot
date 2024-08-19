ee6878d26e9e27bc13565d278d01d5dde2c70f1f
Julian Lechner <fraujulian@lechner.top>
2024-08-19 18:01:02 +0200

    Add files via upload

--- src/Utils/RunSQLInDatabase.js
+++ src/Utils/RunSQLInDatabase.js
@@ -5,14 +5,17 @@ const config = require("../../CONFIGS/config.json")
 
 async function RunSQLInDatabase(commandString, interaction = null) {
     return new Promise((resolve, reject) => {
-        const DatabaseConnection = mysql.createConnection({
+        const DatabaseConnection = mysql.createPool({
             host: config.general.database_data.host,
             port: config.general.database_data.port,
             user: config.general.database_data.username,
             password: config.general.database_data.password,
             database: config.general.database_data.database,
+            waitForConnections: true,
+            enableKeepAlive: true,
+            keepAliveInitialDelay: 0,
         })
-    
+
         DatabaseConnection.query(commandString, async function (err, results, fields) {
             if (
                 err &&
